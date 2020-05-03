88b432b6a1ed808f18d3ef1e8215361b0201ec4c
Ilya Berelson <segrafix@gmail.com>
2020-05-02 23:38:02 -0700

    Update worker.js

--- anigif/jsgif2/worker.js
+++ anigif/jsgif2/worker.js
@@ -1,7 +1,7 @@
 
 
 //var base_url = typeof(base_url_injected)=="undefined"?"":base_url_injected;
-var base_url = 'https://cdn.jsdelivr.net/gh/egfx/GifW00t@54c75d62afe60927b48d7159744d9a4b21cfde3d/anigif/jsgif2/';
+var base_url = 'https://cdn.jsdelivr.net/gh/egfx/GifW00t@7ae1c50c98a5d517d1435755e7ffd66cc6acf3ae/anigif/jsgif2/';
 
 importScripts(base_url + 'GIFEncoder.js', base_url + 'LZWEncoder.js', base_url + 'NeuQuant.js', base_url + 'Demos/b64.js')
 
